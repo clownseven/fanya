6b3e638d2c394dcadc17a7809020d57e6d60c059
clownseven <549958504@qq.com>
2019-10-11 14:07:45 +0900

    second commit

--- fanya/src/main/java/fanya/Dou.java
+++
@@ -1,8 +0,0 @@
-package fanya;
-
-public class Dou {
-	public static void main(String args[]) {
-		System.out.println("小小逗，真可爱");
-	}
-
-}
---
+++ fanya/src/main/java/fanya/Xxd.java
@@ -0,0 +1,11 @@
+package fanya;
+
+public class Xxd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("小小逗真可爱！");
+
+	}
+
+}
